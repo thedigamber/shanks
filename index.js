c5285e69e3b533e256f721793ec25dc4c021340f
Digamber Raj <digamberraj7903@gmail.com>
2026-07-24 21:33:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,19 +5,23 @@ const { BOT_TOKEN } = require('./config');
 const startHandler = require('./handlers/start');
 const joinRequestHandler = require('./handlers/joinRequest');
 
+// Start HTTP server (to satisfy Render port requirement)
+require('./server');
+
+// Create bot instance
 const bot = new Telegraf(BOT_TOKEN);
 
 // Auto‑approve join requests
 bot.on('chat_join_request', joinRequestHandler);
 
-// /start command (only in DM)
+// /start command
 bot.command('start', startHandler);
 
-// Start polling
+// Launch bot (long polling)
 bot.launch()
-  .then(() => console.log('🤖 Bot is running...'))
-  .catch(err => console.error('❌ Launch error:', err));
+  .then(() => console.log('🤖 Bot started successfully'))
+  .catch(err => console.error('❌ Bot launch error:', err));
 
-// Graceful stop
+// Graceful shutdown
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
