ed590bd602e6ad28d815d9a6daf789c63f0a9185
Digamber Raj <digamberraj7903@gmail.com>
2026-07-24 21:31:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   BOT_TOKEN: process.env.BOT_TOKEN,
-  GROUP_ID: parseInt(process.env.GROUP_ID),   // integer
-  GROUP_INVITE_LINK: process.env.GROUP_INVITE_LINK
+  GROUP_ID: parseInt(process.env.GROUP_ID, 10),
+  GROUP_INVITE_LINK: process.env.GROUP_INVITE_LINK,
+  PORT: parseInt(process.env.PORT, 10) || 3000
 };
